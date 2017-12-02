f438221af359d8b256c75f309a970f8841d0fa78
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2017-12-02 16:55:00 +0100

    login page almost done, going on with the index/homepage

---
+++ js/js.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	$('.collpasingSidebar').on('click', function () {
+        $('.sidebar').toggleClass('active');
+    });
+
+    // $('#content').on('click', function () {
+    // 	console.log("eghjkl")
+    //     $('#sidebar.active').removeClass('active');
+    // });
+
+});
